e2fb8cbb7b7db5ea13d73333c56aa4242538f791
Pavel Petrovič <petrovic16@uniba.sk>
2019-07-13 15:07:29 +0200

    measure voltage of battery

---
+++ src/arduino/13_zmeraj_baterku/13_zmeraj_baterku.ino
@@ -0,0 +1,18 @@
+void setup() 
+{
+  Serial.begin(9600);  
+}
+
+float zmeraj_baterku()
+{
+  analogReference(INTERNAL); 
+  float volt = analogRead(A3); 
+  analogReference(DEFAULT);
+  return volt * 0.01181640625;  // volt * 1.1 * 242 / 22 / 1023  (22 KOhm out of 220+22=242 KOhm)
+}
+
+void loop() 
+{
+  float bat = zmeraj_baterku();
+  Serial.println(bat);
+}
