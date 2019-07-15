90267a2eb67e44f2f42869b127eef97968823b84
Pavel Petrovič <petrovic16@uniba.sk>
2019-07-15 09:36:52 +0200

    opravujem vymazavam cast ktora tam bola naviac

--- src/arduino/12_mp3_player/12_mp3_player.ino
+++ src/arduino/12_mp3_player/12_mp3_player.ino
@@ -1,32 +1,4 @@
-Servo l, r;
-int jazda;
-
-void setup() {
-  pinMode(5, INPUT);
-  l.attach(9);
-  r.attach(10);
-  l.write(90);
-  r.write(90);
-  jazda = 0;
-}
-
-void loop() {
-  if (jazda)
-  {
-    if (digitalRead(5) == 1)
-    {
-      l.write(90);
-      r.write(90);
-      delay(500);
-      jazda = 0;
-    }
-  }
-  else
-  {
-    if (digitalRead(5) == 1)
-    {
-      l.write(180);
-      r.write(0);#define MP3_OUTPUT_PIN 5   // connect to Rx pin of DFPlayer
+#define MP3_OUTPUT_PIN 5   // connect to Rx pin of DFPlayer
 #define MP3_BUSY_PIN 8     // connect to BUSY pin of DFPlayer
 
 void setup() 
